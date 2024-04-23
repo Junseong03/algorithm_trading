6a5e7d8496d5a5d3c9ecca5dd3e003be33044b35
Junseong03 <wnstjd995@naver.com>
2024-04-23 15:32:00 +0900

    get_order 메소드 추가

--- api/Kiwoom.py
+++ api/Kiwoom.py
@@ -142,3 +142,16 @@ class Kiwoom(QAxWidget):
 
                     self.balance[code].update({item_name: data})
 
+
+    # kiwoom.py 맨 아래에 작성
+    def get_order(self):
+        self.dynamicCall("SetInputValue(QString, QString)", "계좌번호", self.account_number)
+        self.dynamicCall("SetInputValue(QString, QString)", "전체종목구분", "0")
+        self.dynamicCall("SetInputValue(QString, QString)", "체결구분", "0")
+        self.dynamicCall("SetInputValue(QString, QString)", "매매구분", "0")
+
+        self.dynamicCall("CommRqData(QString, QString, int, QString)", "opt10075_req", "opt10075", 0,  "0002")
+
+        self.tr_event_loop.exec_()
+        return self.tr_data
+    
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = QApplication(sys.argv)
 Kiwoom = Kiwoom()
 
 order_result = Kiwoom.send_order('send_buy_order', 1001, 1, '005930',
-                                 1, 80300, '00')
-
+                                 1, 75700, '00')
+                                                        # ↑ '영웅문' 차트 보면서 가격 수정하기
 
 
 print(order_result)
