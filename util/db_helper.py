5588be468e57bfea523d0149a9941fc898d41e80
Junseong03 <wnstjd995@naver.com>
2024-05-07 17:17:10 +0900

    DB 삽입 예시

--- util/db_helper.py
+++ util/db_helper.py
@@ -2,13 +2,21 @@ import sqlite3
 conn = sqlite3.connect("universe_price.db", isolation_level=None)
 
 cur = conn.cursor()
-cur.execute('''CREATE TABLE balance
-                (
-                code varchar(6) PRIMARY KEY,
-                bid_price int(20) NOT NULL,
-                quantity int(20) NOT NULL,
-                created_at varchar(14) NOT NULL,
-                will_clear_at varchar(14)
-                )''')
+# 드래그 하고 CTAL + /
+# 입력기 변경 단축키 : CTAL + SHIFT
+# cur.execute('''CREATE TABLE balance
+#                 (
+#                 code varchar(6) PRIMARY KEY,
+#                 bid_price int(20) NOT NULL,
+#                 quantity int(20) NOT NULL,
+#                 created_at varchar(14) NOT NULL,
+#                 will_clear_at varchar(14)
+#                 )''')
+
+sql = 'insert into balance(code, bid_price, quantity, created_at, will_clear_at) values(?,?,?,?,?)'
+cur.execute(sql,('000600', 173200, 10, '20240507', 'today'))
+
+print(cur.rowcount)
+
 
 conn.commit()
